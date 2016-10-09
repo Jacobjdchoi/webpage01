75652d211886fda95c0726b0b1755b769c8f3cdc
jaedong Choi <jacobjdchoi@gmail.com>
2016-10-09 14:57:17 +0000

    title bar position fix

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,5 +10,10 @@
 $(document).on('turbolinks:load', function() {
   $(function(){ $(document).foundation(); });
   tinymce.remove();
-  tinymce.init({ selector:'textarea#newsBody', height:"800px" }); 
+  tinymce.init({ selector:'textarea#newsBody', height:"800px" });
+  
+  if ($('#title-bar').css('display') !== "none"){
+    var titleBarHeight = $("#title-bar").outerHeight();
+    $("#pageYield, #offCanvasLeft").css('padding-top', titleBarHeight);
+  }
 });
