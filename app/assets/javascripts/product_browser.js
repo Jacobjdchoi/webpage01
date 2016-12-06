74e189fadb0de7b36c82b544a8467a9dde48a2e4
jacobjdchoi <jacobjdchoi@gmail.com>
2016-12-06 19:52:57 +0900

    styling

--- app/assets/javascripts/product_browser.js
+++ app/assets/javascripts/product_browser.js
@@ -1,26 +1,28 @@
-if ($('#title-bar').css('display') !== "none"){
-// mobile
-  $(document).on('click', '.brand_logo_and_name a', function(){
-    var target = $(this).closest('section'),
-      not_target = $('section').not(target);
-      target.toggleClass('open-vertical');
-      not_target.removeClass('open-vertical');
-      console.log('clicked');
-  });
-} else {
-//web
-  $(document).on('mouseenter', '.brand_items', function(){
-    var target = $(this),
-        not_target = $('.brand_items').not(target);
-      target.addClass('open-horizontal');
-      not_target.addClass('close-horizontal');
-  });
-  $(document).on('mouseleave', '.brand_items', function(){
-    var target = $(this),
-        not_target = $('.brand_items').not(target);
-      target.removeClass('open-horizontal');
-      not_target.removeClass('close-horizontal');
-  });
-}
-
+$(document).on('turbolinks:load', function(){
+  if ($('#title-bar').css('display') !== "none" ){
+  // mobile
+    $(document).on('click', '.brand_logo_and_name a', function(){
+      var target = $(this).closest('section.brand_items'),
+        not_target = $('section').not(target),
+        sign = target.find('i');
+        target.toggleClass('open-vertical');
+        sign.toggleClass('fa-chevron-down fa-chevron-up');
+        not_target.removeClass('open-vertical');
+    });
+  } else {
+  //web
+    $(document).on('mouseenter', '.brand_items', function(){
+      var target = $(this),
+          not_target = $('.brand_items').not(target);
+        target.addClass('open-horizontal');
+        not_target.addClass('close-horizontal');
+    });
+    $(document).on('mouseleave', '.brand_items', function(){
+      var target = $(this),
+          not_target = $('.brand_items').not(target);
+        target.removeClass('open-horizontal');
+        not_target.removeClass('close-horizontal');
+    });
+  }
+})
 
